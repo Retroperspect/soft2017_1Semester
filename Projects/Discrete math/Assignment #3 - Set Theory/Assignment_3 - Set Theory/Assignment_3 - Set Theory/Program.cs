5fb65a2481d1dd1da874b12978e0283e70262b7d
Retroperspect <KristianFlejsborgSorensen@gmail.com>
2017-10-02 20:18:13 +0200

    added #3 DM assignment

---
+++ Projects/Discrete math/Assignment #3 - Set Theory/Assignment_3 - Set Theory/Assignment_3 - Set Theory/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3___Set_Theory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] B = new int[] { 1, 2, 3, 4, 5 };
+            int[] C = new int[] { 5, 6, 7, 8, 9 };
+            int[] D = new int[] { 1, 2, 3, 6, 7, 8 };
+            int[] E = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] F = new int[] { };
+
+            SetAPI API = new SetAPI();
+            Console.WriteLine("intersection of A {0} and B {1} == {2}",string.Join(",",A), string.Join(",", B), string.Join(",",API.Intersection(A, B)));
+            Console.WriteLine("intersection of B {0} and A {1} == {2}", string.Join(",", B), string.Join(",", A), string.Join(",", API.Intersection(B, A)));
+            Console.WriteLine("Union of C {0} and B {1} == {2}", string.Join(",", C), string.Join(",", B), string.Join(",", API.Union(C, B)));
+            Console.WriteLine("Union of B {0} and C {1} == {2}", string.Join(",", B), string.Join(",", C), string.Join(",", API.Union(B, C)));
+            Console.WriteLine("Difference of C {0} too D {1} == {2}", string.Join(",", C), string.Join(",", D), string.Join(",", API.Difference(C, D)));
+            Console.WriteLine("Difference of D {0} too C {1} == {2}", string.Join(",", D), string.Join(",", C), string.Join(",", API.Difference(D, C)));
+            Console.WriteLine("Is B {0} a member of A? {1} == {2}", string.Join(",", B), string.Join(",", A), API.Membership(new HashSet<int>(B), new HashSet<int>(A)));
+            Console.WriteLine("Is A {0} a member of B? {1} == {2}", string.Join(",", A), string.Join(",", B), API.Membership(new HashSet<int>(A), new HashSet<int>(B)));
+            Console.WriteLine("what is the Complement of E {0} too B {1} == {2}", string.Join(",", E), string.Join(",", B), string.Join(",", API.Complement(E, B)));
+            Console.WriteLine("what is the Complement of B {0} too E {1} == {2}", string.Join(",", B), string.Join(",", E), string.Join(",", API.Complement(B, E)));
+            Console.WriteLine("now to compare");
+            Console.WriteLine("Comparing {0} and {1} which is = {2}", string.Join(",", A), string.Join(",", B), API.Comparison(new HashSet<int>(A), new HashSet<int>(B)));
+            Console.WriteLine("Comparing {0} and {1} which is = {2}", string.Join(",", B), string.Join(",", C), API.Comparison(new HashSet<int>(B), new HashSet<int>(C)));
+            Console.WriteLine("Comparing {0} and {1} which is = {2}", string.Join(",", C), string.Join(",", D), API.Comparison(new HashSet<int>(C), new HashSet<int>(D)));
+            Console.WriteLine("Comparing {0} and {1} which is = {2}", string.Join(",", D), string.Join(",", E), API.Comparison(new HashSet<int>(D), new HashSet<int>(E)));
+            Console.WriteLine("Comparing {0} and {1} which is = {2}", string.Join(",", E), string.Join(",", F), API.Comparison(new HashSet<int>(E), new HashSet<int>(F)));
+            Console.WriteLine("Comparing {0} and {1} which is = {2}", string.Join(",", F), string.Join(",", F), API.Comparison(new HashSet<int>(F), new HashSet<int>(F)));
+            Console.WriteLine("Comparing {0} and {1} which is = {2}", string.Join(",", A), string.Join(",", A), API.Comparison(new HashSet<int>(A), new HashSet<int>(A)));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projects/Discrete math/Assignment #3 - Set Theory/Assignment_3 - Set Theory/Assignment_3 - Set Theory/SetAPI.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3___Set_Theory
+{
+    class SetAPI
+    {
+        public SetAPI()
+        {
+
+        }
+
+        /// <summary>
+        /// membership function that checks if X is subset of Y
+        /// </summary>
+        /// <param name="X">Set value to test</param>
+        /// <param name="Y">Set value to test</param>
+        /// <returns>true or false statement</returns>
+        public bool Membership(HashSet<int> X, HashSet<int> Y)
+        {
+            bool statement = false;
+            if (Comparison(X,Y) == -1)
+            {
+                statement = true;
+            }
+            return statement;
+        }
+
+        /// <summary>
+        /// Intersection function
+        /// </summary>
+        /// <param name="X">Set value to test</param>
+        /// <param name="Y">Set value to test</param>
+        /// <returns>All values that both X and Y shares</returns>
+        public int[] Intersection(int[] X, int[] Y)
+        {
+            return X.Intersect(Y).ToArray();
+        }
+
+        /// <summary>
+        /// Union function
+        /// </summary>
+        /// <param name="X">Set value to test</param>
+        /// <param name="Y">Set value to test</param>
+        /// <returns>both X and Y combined</returns>
+        public int[] Union(int[] X, int[] Y)
+        {
+            return X.Union(Y).ToArray();
+        }
+
+        /// <summary>
+        /// Difference fucntion
+        /// </summary>
+        /// <param name="X">Set value to test</param>
+        /// <param name="Y">Set value to test</param>
+        /// <returns>all X without the values that interesect with Y</returns>
+        public int[] Difference(int[] X, int[] Y)
+        {
+            return X.Except(Y).ToArray();
+        }
+
+        /// <summary>
+        /// Complement function
+        /// </summary>
+        /// <param name="X">Set Value to test</param>
+        /// <param name="Y">Contextual value - infinite</param>
+        /// <returns>everything but X</returns>
+        public int[] Complement(int[] X, int[] Y)
+        {
+            return Y.Except(X).ToArray();
+        }
+
+        /// <summary>
+        /// Comparison function that checks the two set values and return a statement value between (-2,-1,0,1,2) depending on the result
+        /// </summary>
+        /// <param name="X">Set value to test</param>
+        /// <param name="Y">Set value to test</param>
+        /// <returns>-2 is default, -1 means X is a subset of Y, 0 means X == Y, 1 means X is a superset of Y, 2 is undetermined</returns>
+        public int Comparison(HashSet<int> X, HashSet<int> Y)
+        {
+            int Z;
+            if (X.IsSubsetOf(Y) && !X.SetEquals(Y))
+            {
+                Z = -1;
+            }
+            else if (X.SetEquals(Y))
+            {
+                Z = 0;
+            }
+            else if (X.IsSupersetOf(Y))
+            {
+                Z = 1;
+            }
+            else if (X.IsSubsetOf(Y) && Y.IsSubsetOf(X))
+            {
+                Z = 2;
+            }
+            else
+            {
+                Z = -2;
+            }
+
+            return Z;
+        }
+
+    }
+}
